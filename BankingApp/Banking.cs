0da9f1507d36748549b30aeca88023aba6ba65db
Karol Morgan <karolyne.morgan@outlook.com>
2024-06-14 14:44:35 -0400

    Add project files.

---
+++ BankingApp/Banking.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp;
+internal class Banking
+{
+    public int AccountId { get; set; }
+    public string Description { get; set; }
+    public decimal Balance { get; set; } = 0;
+
+    public void DepositIntoAccount(decimal amountToAdd)
+    {
+        
+       
+            Balance += amountToAdd;
+            Console.WriteLine($"Sucess! You deposited: ${amountToAdd}");
+        
+    }
+
+    public void WithdrawFromAccount(decimal amountToWithdraw)
+    {
+        
+            Balance -= amountToWithdraw;
+            Console.WriteLine($"Sucess! You withdrawed: ${amountToWithdraw}");
+
+
+    }
+
+    public void DisplayBalance()
+    {
+        Console.WriteLine($"{Description} Account Balance: ${Balance}");
+    }
+
+
+
+
+
+}
---
+++ BankingApp/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace BankingApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Banking myAccount = new Banking { AccountId = 1, Description = "Checking" };
+
+        Console.WriteLine("Welcome to Karol's bank!");
+        Console.WriteLine("Type AccountId: ");
+        var accountId = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Type Checking or Savings: ");
+        var accountDescription = Console.ReadLine();
+
+        Banking myAccount = new Banking { AccountId = accountId, Description = accountDescription };
+
+        
+
+        while (true)
+        {
+            Console.WriteLine($"Account Id: {myAccount.AccountId} Type: {myAccount.Description}");
+            Console.WriteLine("Choose an operation (deposit, withdraw or balance) or type 'cancel': ");
+            var operation = Console.ReadLine();
+
+            if (operation == "balance")
+            {
+                myAccount.DisplayBalance();
+            }
+            else if (operation == "deposit" || operation == "withdraw")
+            {
+                Console.WriteLine("How much?");
+                decimal money = Convert.ToDecimal(Console.ReadLine());
+                if (money < 0)
+                {
+                    Console.WriteLine("Try again.");
+                    continue;
+                } else
+                {
+                    if (operation == "deposit")
+                    {
+                        myAccount.DepositIntoAccount(money);
+
+                    }
+                    else if (operation == "withdraw" && money < myAccount.Balance)
+                    {
+                        myAccount.WithdrawFromAccount(money);
+
+                    } else
+                    {
+                        Console.WriteLine("Try again.");
+                            continue;
+                    }
+                }
+                
+            } else if (operation == "cancel")
+            {
+                break;
+            }
+        }
+        
+    }
+}
